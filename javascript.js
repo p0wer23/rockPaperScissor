fe219c115e88d920cc4e8804698262f69f50423f
Punith K <punithk03@gmail.com>
2022-12-09 10:20:31 +0530

    Change of javascript.js for converting the game to UI. initial

--- javascript.js
+++ javascript.js
@@ -1,43 +1,47 @@
+const btns = Array.from(document.querySelectorAll('button'));
+let playerScore = 0;
+let computerScore = 0;
+
+//Identifies when buttons are clicked
+btns.forEach( (btn) => {btn.addEventListener('click',(e) => playRound(e.target.textContent.toLowerCase()));} );
+
 //returns rock, paper or scissor randomly.
 function getComputerChoice()
 {
     let choices=["Rock", "Paper", "Scissor"];
-
     return choices[Math.floor(Math.random()*3)];
 }
 
-//one round is played winner is returned
-function game()
+//one round is played
+function playRound(playerSelection)
 {
-    const playerSelection = prompt("Enter Your Choice: 'Rock', 'Paper' or 'Scissor'").toLowerCase().trim();
     const computerSelection = getComputerChoice().toLowerCase();
+    winner = returnWinner(playerSelection, computerSelection);
 
+    if (winner==='player')
+        playerScore++;
+    else if (winner==='computer')
+        computerScore++;
+    update(playerSelection, computerSelection, winner, playerScore, computerScore);
+
+    if (playerScore===5)
+        console.log('Player Wins');
+    else if (computerScore==5)
+        console.log('Computer Wins')
+}
+
+function update(playerSelection, computerSelection, winner, playerScore, computerScore)
+{
+    console.log(playerSelection, computerSelection, winner, playerScore, computerScore);
+}
+
+
+//winner is returned
+function returnWinner(playerSelection,computerSelection)
+{
     if (playerSelection === computerSelection)
         return 'none';
     if ( (playerSelection==='rock' && computerSelection==='scissor') || (playerSelection==='paper' && computerSelection==='rock') || (playerSelection==='scissor' && computerSelection==='paper') )
         return 'player';
     return 'computer';
 }
-
-
-
-let playerScore = 0;
-let computerScore = 0;
-
-for (let i=0; i<5; i++)
-{
-
-    let result = game();
-    if (result==='player')
-        playerScore++;
-    else if (result==='computer')
-        computerScore++;
-
-}
-  
-if (playerScore > computerScore)
-    alert(`Congratulations!!! You win by ${playerScore} : ${computerScore}.`);
-else if (computerScore > playerScore)
-    alert(`You lose. Computer wins by ${computerScore} : ${playerScore}.`);
-else
-    alert(`So cloose. Its a tie. Score: ${computerScore} : ${playerScore}.`)
