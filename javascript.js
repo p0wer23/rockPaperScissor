5c3fd1cdf9afe3d740e0c97faf667f9c814bae51
Punith K <punithk03@gmail.com>
2023-04-13 23:00:43 +0530

    Add images and play again features

--- javascript.js
+++ javascript.js
@@ -41,13 +41,45 @@ function update(playerSelection, computerSelection, winner, playerScore, compute
     const winnerPara = document.querySelector(".winner");
     const playerScorePara = document.querySelector(".playerScore");
     const computerScorePara = document.querySelector(".computerScore");
+    const playerChoiceImage = document.querySelector("#playerChoiceImage");
+    const computerChoiceImage = document.querySelector("#computerChoiceImage");
+    console.log(playerChoiceImage)
+    console.log(computerChoiceImage)
 
-    roundPara.textContent = `Round = ${round}`;
-    playerChoicePara.textContent = `Player Chooses = ${playerSelection}`;
-    computerChoicePara.textContent = `Computer Chooses = ${computerSelection}`;
+    //Text Update
+    roundPara.textContent = `Round: ${round}`;
+    playerChoicePara.textContent = `Player Chooses: ${playerSelection}`;
+    computerChoicePara.textContent = `Computer Chooses: ${computerSelection}`;
     winnerPara.textContent = `Winner of this round: ${winner}`;
     playerScorePara.textContent = `Player Scoore: ${playerScore}`;
     computerScorePara.textContent = `Computer Score: ${computerScore}`;
+
+    //Images Update
+    const pimg = document.createElement('img');
+    if (playerSelection === 'rock')
+        pimg.src = './images/rock.png';
+    else if (playerSelection === 'paper')
+        pimg.src = './images/paper.png';
+    else
+        pimg.src = './images/scissor.png';
+    while(playerChoiceImage.firstChild)
+        playerChoiceImage.removeChild(playerChoiceImage.firstChild);
+    pimg.style.height = '100%';
+    pimg.style.width = '100%';
+    playerChoiceImage.appendChild(pimg);
+
+    const cimg = document.createElement('img');
+    if (computerSelection === 'rock')
+        cimg.src = './images/rock.png';
+    else if (computerSelection === 'paper')
+        cimg.src = './images/paper.png';
+    else
+        cimg.src = './images/scissor.png';
+    while(computerChoiceImage.firstChild)
+        computerChoiceImage.removeChild(computerChoiceImage.firstChild);
+    cimg.style.width = '100%';
+    cimg.style.height = '100%';
+    computerChoiceImage.appendChild(cimg);
 }
 
 //winner is returned
@@ -60,3 +92,31 @@ function returnWinner(playerSelection,computerSelection)
     return 'computer';
 }
 
+function declareWinner(winner)
+{
+    if (winner==='Player')
+    {
+        if (window.confirm('Congratulations!!!! You Won!!!\n Do you want to play again?'))
+            location.reload()
+        else
+        {
+            while( document.querySelector(".selection").firstChild ) 
+                document.querySelector(".selection").removeChild(document.querySelector(".selection").firstChild);
+            document.querySelector('h4').innerHTML='';
+            document.querySelector('.winner').innerHTML=`You have won the Match`;
+        }
+
+    }
+    else
+    {
+        if(window.confirm('You Lost. Better Luck next time.\n Do you want to play again?'))
+            location.reload()
+        else
+        {
+            while( document.querySelector(".selection").firstChild ) 
+                document.querySelector(".selection").removeChild(document.querySelector(".selection").firstChild);
+            document.querySelector('h4').innerHTML='';
+            document.querySelector('.winner').innerHTML=`Compuer won the Match`;
+        }
+    }
+}
