d72059617b3ae5b5282ec4357061b6a120f98075
Punith K <punithk03@gmail.com>
2023-04-15 17:00:08 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,6 +6,12 @@ let round = 0;
 //Identifies when buttons are clicked
 btns.forEach( (btn) => {btn.addEventListener('click',(e) => playRound(e.target.textContent.toLowerCase()));} );
 
+outcomes = {
+    'rock': {'rock':'tie', 'paper':'computer', 'scissor':'player'},
+    'paper': {'rock':'player', 'paper':'tie', 'scissor':'computer'},
+    'scissor': {'rock':'computer', 'paper':'player', 'scissor':'tie'}
+}
+
 //returns rock, paper or scissor randomly.
 function getComputerChoice()
 {
@@ -17,34 +23,32 @@ function getComputerChoice()
 function playRound(playerSelection)
 {
     const computerSelection = getComputerChoice().toLowerCase();
-    winner = returnWinner(playerSelection, computerSelection);
+    let winner = outcomes[playerSelection][computerSelection];
 
     if (winner==='player')
         playerScore++;
     else if (winner==='computer')
         computerScore++;
     round++;
-    update(playerSelection, computerSelection, winner, playerScore, computerScore, round);
+
+    updateText(playerSelection, computerSelection, winner, playerScore, computerScore, round);
+    updateImages(playerSelection, computerSelection);
 
     if (playerScore===5)
-        declareWinner('Player');
+        setTimeout(()=>{declareWinner('Player');},50);
     else if (computerScore==5)
-        declareWinner('Computer');
+        setTimeout(()=>{declareWinner('Computer');},50);
 }
 
 //Updates the current round choices, winner and score.
-function update(playerSelection, computerSelection, winner, playerScore, computerScore, round)
+function updateText(playerSelection, computerSelection, winner, playerScore, computerScore, round)
 {
-    const roundPara = document.querySelector(".round")
+    const roundPara = document.querySelector(".round");
     const playerChoicePara = document.querySelector(".playerChoice");
     const computerChoicePara = document.querySelector(".computerChoice");
     const winnerPara = document.querySelector(".winner");
     const playerScorePara = document.querySelector(".playerScore");
     const computerScorePara = document.querySelector(".computerScore");
-    const playerChoiceImage = document.querySelector("#playerChoiceImage");
-    const computerChoiceImage = document.querySelector("#computerChoiceImage");
-    console.log(playerChoiceImage)
-    console.log(computerChoiceImage)
 
     //Text Update
     roundPara.textContent = `Round: ${round}`;
@@ -53,15 +57,17 @@ function update(playerSelection, computerSelection, winner, playerScore, compute
     winnerPara.textContent = `Winner of this round: ${winner}`;
     playerScorePara.textContent = `Player Scoore: ${playerScore}`;
     computerScorePara.textContent = `Computer Score: ${computerScore}`;
+}
+
+function updateImages(playerSelection, computerSelection)
+{
+    const playerChoiceImage = document.querySelector("#playerChoiceImage");
+    const computerChoiceImage = document.querySelector("#computerChoiceImage");
 
     //Images Update
     const pimg = document.createElement('img');
-    if (playerSelection === 'rock')
-        pimg.src = './images/rock.png';
-    else if (playerSelection === 'paper')
-        pimg.src = './images/paper.png';
-    else
-        pimg.src = './images/scissor.png';
+    pimg.src = `./images/${playerSelection}.png`
+
     while(playerChoiceImage.firstChild)
         playerChoiceImage.removeChild(playerChoiceImage.firstChild);
     pimg.style.height = '100%';
@@ -69,27 +75,13 @@ function update(playerSelection, computerSelection, winner, playerScore, compute
     playerChoiceImage.appendChild(pimg);
 
     const cimg = document.createElement('img');
-    if (computerSelection === 'rock')
-        cimg.src = './images/rock.png';
-    else if (computerSelection === 'paper')
-        cimg.src = './images/paper.png';
-    else
-        cimg.src = './images/scissor.png';
+    cimg.src = `./images/${computerSelection}.png`
+
     while(computerChoiceImage.firstChild)
         computerChoiceImage.removeChild(computerChoiceImage.firstChild);
     cimg.style.width = '100%';
     cimg.style.height = '100%';
     computerChoiceImage.appendChild(cimg);
-}
-
-//winner is returned
-function returnWinner(playerSelection,computerSelection)
-{
-    if (playerSelection === computerSelection)
-        return 'tie';
-    if ( (playerSelection==='rock' && computerSelection==='scissor') || (playerSelection==='paper' && computerSelection==='rock') || (playerSelection==='scissor' && computerSelection==='paper') )
-        return 'player';
-    return 'computer';
 }
 
 function declareWinner(winner)
@@ -99,24 +91,17 @@ function declareWinner(winner)
         if (window.confirm('Congratulations!!!! You Won!!!\n Do you want to play again?'))
             location.reload()
         else
-        {
-            while( document.querySelector(".selection").firstChild ) 
-                document.querySelector(".selection").removeChild(document.querySelector(".selection").firstChild);
-            document.querySelector('h4').innerHTML='';
             document.querySelector('.winner').innerHTML=`You have won the Match`;
-        }
-
     }
     else
     {
         if(window.confirm('You Lost. Better Luck next time.\n Do you want to play again?'))
             location.reload()
         else
-        {
-            while( document.querySelector(".selection").firstChild ) 
-                document.querySelector(".selection").removeChild(document.querySelector(".selection").firstChild);
-            document.querySelector('h4').innerHTML='';
             document.querySelector('.winner').innerHTML=`Computer won the Match`;
-        }
     }
+
+    while( document.querySelector(".selection").firstChild ) 
+        document.querySelector(".selection").removeChild(document.querySelector(".selection").firstChild);
+    document.querySelector('h4').innerHTML='';
 }
